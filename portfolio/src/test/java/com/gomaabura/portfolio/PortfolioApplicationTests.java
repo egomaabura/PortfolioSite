96beae02628e55afaad48c4473889f913a6b8457
egomaabura <k1222ars@gmail.com>
2025-04-04 22:38:58 +0900

    initial commit

---
+++ portfolio/src/test/java/com/gomaabura/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomaabura.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
